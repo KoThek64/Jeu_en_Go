4f5f05e3196a6bc01a39af320fc4d79aaffc1cba
E244845 <axel.doucet@etu.univ-nantes.fr>
2024-12-18 17:41:38 +0100

    Ajout de GetContent + documentations sur fonctions make

--- quadtree/get.go
+++ quadtree/get.go
@@ -1,8 +1,56 @@
 package quadtree
 
-// GetContent remplit le tableau contentHolder (qui représente
-// un terrain dont la case le plus en haut à gauche a pour coordonnées
-// (topLeftX, topLeftY)) à partir du qadtree q.
-func (q Quadtree) GetContent(topLeftX, topLeftY int, contentHolder [][]int) {
-	// TODO
+func nextNode(j int, i int, noeud node) (next_node node) {
+	if j < noeud.bottomLeftNode.topLeftY {
+		//la case est dans la partie superieur du quadtree
+		if i < noeud.topRightNode.topLeftX {
+			return *noeud.topLeftNode
+		} else {
+			return *noeud.topRightNode
+		}
+	} else {
+		//la case est dans la partie inferieur du quadtree
+		if i < noeud.bottomRightNode.topLeftX {
+			return *noeud.bottomLeftNode
+		} else {
+			return *noeud.bottomRightNode
+		}
+	}
+}
+
+// GetContent remplit le tableau contentHolder (qui reprÃ©sente
+// un terrain dont la case le plus en haut Ã  gauche a pour coordonnÃ©es
+// (topLeftX, topLeftY)) Ã  partir du qadtree q.
+func (q Quadtree) GetContent(topLeftX, topLeftY int, contentHolder [][]int) { //Simon
+	// recuperation du noeud racine
+	var racine node = *q.root
+
+	//parcours des cases du tableau
+	for i := 0; i < len(contentHolder); i++ {
+		for j := 0; j < len(contentHolder[i]); j++ {
+			//current_node : noeud sur lequel on travaille
+			var current_node node = racine
+
+			//Y, X: conversion de la position realtive dans contentHolder Ã  une position absolue dans le quadtree
+			var Y int = i + topLeftY
+			var X int = j + topLeftX
+
+			//verification que la case relative ne soit pas hors du quadtree
+			var out_of_map bool = (Y < current_node.topLeftY ||
+				Y > (current_node.topLeftY+current_node.height-1) ||
+				X < current_node.topLeftX ||
+				X > (current_node.topLeftX+current_node.width-1))
+
+			if out_of_map {
+				contentHolder[i][j] = -1
+			} else {
+
+				//recherche du leaf dans lequel se trouve la case
+				for !current_node.isLeaf {
+					current_node = nextNode(Y, X, current_node)
+				}
+				contentHolder[i][j] = current_node.content
+			}
+		}
+	}
 }
--- quadtree/make.go
+++ quadtree/make.go
@@ -1,54 +1,61 @@
 package quadtree
 
+// makeFromArrayRecursive construit un nœud de quadtree de manière récursive à partir d'un tableau 2D donné.
+// - floorContent : le tableau 2D représentant le terrain.
+// - topLeftX, topLeftY : coordonnées du coin supérieur gauche de la zone actuelle du nœud.
+// - width, height : dimensions de la zone actuelle du nœud.
 func makeFromArrayRecursive(floorContent [][]int, topLeftX, topLeftY, width, height int) *node {
-
+	// Supposer initialement que la zone actuelle a un contenu uniforme
 	content := floorContent[topLeftY][topLeftX]
 	is_leaf := true
 
-	check_leaf: for x := topLeftX; x < topLeftX + width; x ++ {
-		for y := topLeftY; y < topLeftY + height; y ++ {
+	// Vérifier si la zone n'est pas uniforme
+check_leaf:
+	for x := topLeftX; x < topLeftX+width; x++ {
+		for y := topLeftY; y < topLeftY+height; y++ {
 			if floorContent[y][x] != content {
 				is_leaf = false
 				break check_leaf
 			}
-
 		}
 	}
 
+	// Créer un nouveau nœud de quadtree
 	returnNode := node{
 		topLeftX: topLeftX,
 		topLeftY: topLeftY,
-		width: width,
-		height: height,
-		isLeaf: is_leaf,
+		width:    width,
+		height:   height,
+		isLeaf:   is_leaf,
 	}
 
-
+	// Si la zone est uniforme, assigner le contenu
 	if is_leaf {
 		returnNode.content = content
-	}else {
+	} else {
+		// Diviser la zone en quatre quadrants et créer les nœuds enfants
 		middleWidth := width / 2
 		middleHeight := height / 2
 
 		returnNode.topLeftNode = makeFromArrayRecursive(floorContent, topLeftX, topLeftY, middleWidth, middleHeight)
-		returnNode.topRightNode = makeFromArrayRecursive(floorContent, topLeftX + middleWidth, topLeftY, width - middleWidth, middleHeight)
-		returnNode.bottomLeftNode = makeFromArrayRecursive(floorContent, topLeftX, topLeftY + middleHeight, middleWidth, height - middleHeight)
-		returnNode.bottomRightNode = makeFromArrayRecursive(floorContent, topLeftX + middleWidth, topLeftY + middleHeight, width - middleWidth, height - middleHeight)
+		returnNode.topRightNode = makeFromArrayRecursive(floorContent, topLeftX+middleWidth, topLeftY, width-middleWidth, middleHeight)
+		returnNode.bottomLeftNode = makeFromArrayRecursive(floorContent, topLeftX, topLeftY+middleHeight, middleWidth, height-middleHeight)
+		returnNode.bottomRightNode = makeFromArrayRecursive(floorContent, topLeftX+middleWidth, topLeftY+middleHeight, width-middleWidth, height-middleHeight)
 	}
 
 	return &returnNode
 }
 
-// MakeFromArray construit un quadtree représentant un terrain
-// étant donné un tableau représentant ce terrain.
+// MakeFromArray construit un quadtree à partir d'un tableau 2D représentant un terrain.
+// - floorContent : le tableau 2D à convertir en quadtree.
 func MakeFromArray(floorContent [][]int) Quadtree {
-	
-	width := len(floorContent[0])
-	height := len(floorContent)
+	width := len(floorContent[0])  // Obtenir la largeur du terrain
+	height := len(floorContent)    // Obtenir la hauteur du terrain
 
+	// Retourner le quadtree avec son nœud racine initialisé récursivement
 	return Quadtree{
-		width: width,
+		width:  width,
 		height: height,
-		root: makeFromArrayRecursive(floorContent, 0, 0, width, height),
+		root:   makeFromArrayRecursive(floorContent, 0, 0, width, height),
 	}
 }
